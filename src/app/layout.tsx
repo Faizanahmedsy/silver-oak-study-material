7d6aaf8f491e5b96eb20f22d9788c21cf00173b7
Faizanahmedsy <saiyed.faizanahmed1@gmail.com>
2024-03-29 15:12:08 +0530

    added google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Providers from "@/components/providers";
 import Navbar from "@/components/modules/landing/navbar/navbar";
 import Footer from "@/components/modules/landing/footer/footer";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* <!-- Google tag (gtag.js) --> */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-B3WRLMME0D"
+        ></Script>
+        <Script id="google-analytics">
+          {`
+           window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
+           gtag('config', 'G-B3WRLMME0D');
+          `}
+        </Script>
+      </head>
       <body className={inter.className}>
         <Providers>
           {/* <Navbar /> */}
