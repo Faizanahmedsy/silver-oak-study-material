5f9b9ac1bf2ad596d6c2b4f192965f8918b95c64
Faizanahmedsy <saiyed.faizanahmed1@gmail.com>
2024-03-29 15:02:32 +0530

    made android app

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.soustudybuddy.app",
+  appName: "sou-study-buddy",
+  webDir: "out",
+  server: {
+    androidScheme: "https",
+  },
+};
+
+export default config;
